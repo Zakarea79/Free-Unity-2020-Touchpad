a7b8eb80bbc2a56f2b5f5ef67914444f1f296f62
unknown <zakarea6688@gmail.com>
2022-02-04 14:50:55 +0330

    Cleaning the project structure

---
+++ Assets/Supernova/Example/Sample.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    void Update()
+    {
+        float x = ZInput.GetAxis("X");
+        float y = ZInput.GetAxis("Y");
+        transform.Translate(new Vector3(x, y , 0) * 5 * Time.deltaTime);
+        //-------------------------------------------------------------
+        if(ZInput.GetKeyDown("button1"))
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
+        }
+    }
+}
